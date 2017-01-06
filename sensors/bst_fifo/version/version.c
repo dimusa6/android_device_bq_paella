a8cbe77c901e75f856f8ebc3018bbce09d81e086
Darkmet98 <darkmet98@glowtranslations.tk>
2017-01-06 15:39:07 +0100

    Fixed all sensors

--- sensors/bst_fifo/version/version.c
+++ sensors/bst_fifo/version/version.c
@@ -5,7 +5,7 @@
 
 static const char daemon_version_str[] = "3.0.39";
 static const char hal_version_str[] = "3.0.17";
-static const char date_str[] = "2016-11-10 22:00:45";
+static const char date_str[] = "2017-01-06 13:04:08";
 const char* get_daemon_version(void)
 {
 	return daemon_version_str;
